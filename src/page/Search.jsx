9c65dd48f16f6df50a38e5042e4aa162cc7778d6
azka <mohazkalarif@gmail.com>
2022-12-21 07:50:40 +0700

    responsive

--- src/page/Search.jsx
+++ src/page/Search.jsx
@@ -1,88 +1,87 @@
-import { Dialog, Transition } from '@headlessui/react'
-import Footer from '../components/Footer'
-import { Fragment, useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Dialog, Transition } from "@headlessui/react";
+import Footer from "../components/Footer";
+import { Fragment, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Search() {
-    let [isOpen, setIsOpen] = useState(false)
-    const [animes, setAnimes] = useState([])
-    const params = useParams()
-    const [isLoading, setIsLoading] = useState(true)
+  let [isOpen, setIsOpen] = useState(false);
+  const [animes, setAnimes] = useState([]);
+  const params = useParams();
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        setIsLoading(true)
-        axios(
-            `https://encouraging-bat-sun-hat.cyclic.app/api/anime/oploverz/search?q=${params.query}`
-        ).then((res) => {
-            setAnimes(res.data.results)
-            setIsLoading(false)
-        })
-    }, [params])
+  useEffect(() => {
+    setIsLoading(true);
+    axios(
+      `https://encouraging-bat-sun-hat.cyclic.app/api/anime/oploverz/search?q=${params.query}`
+    ).then((res) => {
+      setAnimes(res.data.results);
+      setIsLoading(false);
+    });
+  }, [params]);
 
-    function closeModal() {
-        setIsOpen(false)
-    }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-    function openModal() {
-        setIsOpen(true)
-    }
-    if (isLoading) {
-        return (
-            <div className='grid h-screen place-items-center'>
-                <div className='flex items-center'>
-                    <div className='w-24 h-24 border-t-4 border-b-4 border-slate-900 dark:border-red-900 rounded-full animate-spin'></div>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <>
-                <div>
-                    <div className='w-full text-slate-900 dark:text-white mt-6 max-w-5xl bg-slate-100 dark:bg-slate-700 rounded-md overflow-hidden mx-auto'>
-                        <h2 className='font-semibold block border-b border-slate-300 dark:border-slate-500  py-2 px-4'>{`Search '${params.query}'`}</h2>
-                        <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4  px-4 py-2'>
-                            {animes.map((anime, index) => {
-                                return (
-                                    <div key={index}>
-                                        <Link to={`/anime/${anime.id}`}>
-                                            {' '}
-                                            <div className='flex flex-column w-full relative'>
-                                                <img
-                                                    className='aspect-auto w-full object-cover h-full'
-                                                    src={anime.img}
-                                                    alt=''
-                                                />
-                                                <div
-                                                    className={`absolute right-0 m-2 px-1 rounded text-sm ${
-                                                        anime.type === 'tv'
-                                                            ? 'bg-blue-600'
-                                                            : anime.type ===
-                                                              'Special'
-                                                            ? 'bg-red-500'
-                                                            : 'bg-blue-500'
-                                                    }`}
-                                                >
-                                                    {anime.type}
-                                                </div>
-                                                {/* <div className='absolute bottom-0 bg-gradient-to-t from-black w-full p-2 pt-5'>
+  function openModal() {
+    setIsOpen(true);
+  }
+  if (isLoading) {
+    return (
+      <div className="grid h-screen place-items-center">
+        <div className="flex items-center">
+          <div className="w-24 h-24 border-t-4 border-b-4 border-slate-900 dark:border-red-900 rounded-full animate-spin"></div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className="mx-3">
+          <div className="w-full text-slate-900 dark:text-white mt-6 max-w-5xl bg-slate-100 dark:bg-slate-700 rounded-md overflow-hidden mx-auto">
+            <h2 className="font-semibold block border-b border-slate-300 dark:border-slate-500  py-2 px-4">{`Search '${params.query}'`}</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4  px-4 py-2">
+              {animes.map((anime, index) => {
+                return (
+                  <div key={index}>
+                    <Link to={`/anime/${anime.id}`}>
+                      {" "}
+                      <div className="flex flex-column w-full relative">
+                        <img
+                          className="aspect-auto w-full object-cover h-full"
+                          src={anime.img}
+                          alt=""
+                        />
+                        <div
+                          className={`absolute right-0 m-2 px-1 rounded text-sm ${
+                            anime.type === "tv"
+                              ? "bg-blue-600"
+                              : anime.type === "Special"
+                              ? "bg-red-500"
+                              : "bg-blue-500"
+                          }`}
+                        >
+                          {anime.type}
+                        </div>
+                        {/* <div className='absolute bottom-0 bg-gradient-to-t from-black w-full p-2 pt-5'>
                                             oke
                                         </div> */}
-                                            </div>
-                                            <div className='w-full text-center text-sm py-2'>
-                                                {anime.title}
-                                            </div>
-                                        </Link>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
+                      </div>
+                      <div className="w-full text-center text-sm py-2">
+                        {anime.title}
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
 
-                    <Footer />
-                </div>
-                {/* <div className='fixed inset-0 flex items-center justify-center'>
+          <Footer />
+        </div>
+        {/* <div className='fixed inset-0 flex items-center justify-center'>
                 <button
                     type='button'
                     onClick={openModal}
@@ -155,7 +154,7 @@ export default function Search() {
                     </div>
                 </Dialog>
             </Transition> */}
-            </>
-        )
-    }
+      </>
+    );
+  }
 }
